f593fa8edd4a1483e1aa6873d3098542e6aa69db
typhonrt <support@typhonjs.io>
2022-03-26 02:12:53 -0700

    refactor configuration

--- src/test/init.js
+++ src/test/init.js
@@ -6,18 +6,12 @@ import Plugin from './plugin/Plugin.js';
 // Initialize TinyMCE WC plugin.
 Plugin();
 
-Hooks.on('init', () =>
-{
-   game.settings.registerMenu('typhonjs-webcomponent', 'config', {
-      name: 'Click to open:',
-      label: 'Web Component Test',
-      icon: 'fas fa-palette',
-      type: SettingsShim,
-      restricted: false
-   });
-});
+Hooks.on('init', () => game.settings.registerMenu('typhonjs-webcomponent', 'config', {
+   name: 'Click to open:',
+   label: 'Web Component Test',
+   icon: 'fas fa-palette',
+   type: SettingsShim,
+   restricted: false
+}));
 
-Hooks.on('TyphonJS.WCTest.Open', () =>
-{
-   openTestApp();
-});
+Hooks.on('TyphonJS.WCTest.Open', openTestApp);
--- src/test/plugin/Plugin.js
+++ src/test/plugin/Plugin.js
@@ -1,5 +1,10 @@
 import { appendConfigString } from './appendConfigString.js';
 
+// An object containing the available tags as keys w/ text description as value.
+const s_TAGS = {
+   'wc-doc-stat-block': 'Document Stat Block'
+}
+
 export class PluginWC
 {
    /**
@@ -7,24 +12,37 @@ export class PluginWC
     */
    constructor(editor)
    {
-      editor.addCommand('typhonjsWC', () => editor.insertContent('<wc-doc-stat-block>&nbsp;</wc-doc-stat-block>'));
-
-      // Register button.
-      editor.ui.registry.addToggleButton('typhonjs-wc', {
-         tooltip: 'Insert stat block',
+      // Register a custom toolbar menu button to insert web component tags.
+      // https://www.tiny.cloud/docs/ui-components/typesoftoolbarbuttons/#menubutton
+      editor.ui.registry.addMenuButton('typhonjs-wc', {
          icon: 'embed-page',
-         onAction: () => editor.execCommand('typhonjsWC')
+         tooltip: 'Insert Web Component',
+         fetch: (callback) => {
+            // Add menu items to insert web component tag at the location of the cursor.
+            const items = Object.keys(s_TAGS).map((tag) => {
+               return {
+                  type: 'menuitem',
+                  text: s_TAGS[tag],
+                  onAction: () => editor.insertContent(`<${tag}>&nbsp;</${tag}>`)
+               }
+            });
+            callback(items);
+         }
       });
 
-      editor.settings.extended_valid_elements = appendConfigString(editor.settings.extended_valid_elements,
-       'wc-doc-stat-block[*]');
+      // Append tags to `extended_valid_elements` and `custom_elements` TinyMCE settings.
+      for (const tag in s_TAGS)
+      {
+         editor.settings.extended_valid_elements = appendConfigString(editor.settings.extended_valid_elements,
+          `${tag}[*]`);
 
-      editor.settings.custom_elements = appendConfigString(editor.settings.custom_elements, 'wc-doc-stat-block');
+         editor.settings.custom_elements = appendConfigString(editor.settings.custom_elements, `${tag}`);
+      }
 
+      // Get the iframe window object and the iframes document object and initialize it with Foundry globals and
+      // load the web components.
       editor.on('init', () =>
       {
-         // Get the iframe window object and the iframes document object
-         // and call our setup function that creates the web component
          // https://www.tiny.cloud/docs/api/tinymce/tinymce.editor/#getwin
          // https://www.tiny.cloud/docs/api/tinymce/tinymce.editor/#getdoc
          const win = editor.getWin();
@@ -44,11 +62,15 @@ export class PluginWC
          doc.head.append(script);
       });
 
-      // The preinit event is fired after the editor is loaded but before
-      // the content is loaded
+      // The preinit event is fired after the editor is loaded but before the content is loaded.
+      // Add node filters to add / remove the `active` and `contenteditable` attributes.
       // https://www.tiny.cloud/docs/advanced/events/#editorcoreevents
       editor.on('preinit', () => {
-         editor.parser.addNodeFilter('wc-doc-stat-block', (nodes) =>
+         const tags = Object.keys(s_TAGS).join(',');
+
+         // During the creation of the web component we set the contenteditable attribute to false making it behave
+         // like a noneditable but selectable element inside TinyMCE.
+         editor.parser.addNodeFilter(tags, (nodes) =>
          {
             for (const node of nodes)
             {
@@ -57,16 +79,10 @@ export class PluginWC
             }
          });
 
-
-         // During the creation of the web component we set contenteditable false
-         // on the web component to make it behave like a noneditable but selectable
-         // element inside TinyMCE. But we don't want the contenteditable attribute
-         // to be saved with the content. We therefore need to filter out the attribute
-         // upon serialization (which happens on "save", view sourcecode and preview
-         // among others).
+         // On serialization remove `active` and `contenteditable` attributes.
+         // Serialization occurs on "save", view sourcecode and preview, etc.
          // https://www.tiny.cloud/docs/api/tinymce.dom/tinymce.dom.serializer/#addnodefilter
-         editor.serializer.addNodeFilter('wc-doc-stat-block', (nodes) => {
-            // Iterate through all filtered nodes and remove the active and contenteditable attributes.
+         editor.serializer.addNodeFilter(tags, (nodes) => {
             for (const node of nodes)
             {
                if (!!node.attr('active')) { node.attr('active', null); }
